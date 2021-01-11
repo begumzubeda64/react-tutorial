2b7ad26dfa7ff935d47fb909536e6d5ad958a3c5
Zubeda Abbas <bzubeda64@gmail.com>
2021-01-12 00:28:39 +0530

    React with babel webpack

---
+++ p1/src/cart/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Cart extends React.Component {
+    render(){
+        //console.log(this.props.name); passed from parent component
+        return (
+            <div>
+                <h1>Cart</h1>
+            </div>
+        )
+    }
+}
+
+export default Cart;
