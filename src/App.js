a8dababcde9a56dce703f53b2ff4642a2b550686
Zubeda Abbas <bzubeda64@gmail.com>
2021-01-12 00:40:11 +0530

    hello-world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './child';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [],
+    task: ''
+  }
+
+  removeItem(index){
+    const todoList = this.state.todos;
+    todoList.splice(index, 1);
+    this.setState({ todos: todoList });
+  }
+
+  addNewTodo(){
+    this.setState(prevState => ({ 
+      todos: prevState.todos.concat(this.state.task)
+    }));
+  }
+
+  handleChange(e){
+    const value = e.target.value;
+    this.setState({
+      task: value
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input onChange={(e) => this.handleChange(e)} type="text" value={this.state.task} />
+          <button onClick={() => this.addNewTodo()}>Add Task</button>
+          <TodoList removeItem={(index) => this.removeItem(index)} tasks={this.state.todos} />
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
